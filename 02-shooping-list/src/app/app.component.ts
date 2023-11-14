eb6d5590ddf790afc5ab7d402f123ae1b3409664
portoman <portbuj@yahoo.es>
2023-11-14 08:39:13 +0100

    Nav with conditional

--- 02-shooping-list/src/app/app.component.ts
+++ 02-shooping-list/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '02-shooping-list';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
