bb937ad8afb980a545d7694af5b5e8e802d2915a
portoman <portbuj@yahoo.es>
2023-11-21 10:31:04 +0100

    Servicios

--- 02-shooping-list/src/app/shopping-list/shopping-list.component.ts
+++ 02-shooping-list/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Ingredient } from '../shared/ingredients.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
