d2c1b68183e58e30ba4e61aba75b3522eaf656d3
portoman <portbuj@yahoo.es>
2023-11-22 17:51:09 +0100

    Services

--- 02-shooping-list/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 02-shooping-list/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
+  constructor(private slService: ShoppingListService) {}
+
+  ngOnInit() {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    if (ingName === '' || ingAmount === '') return;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
+  }
 }
